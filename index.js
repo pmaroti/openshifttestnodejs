342d87624363a536ccf7e5a5b34aa1ebfa20527d
maro <pmaroti@gmail.com>
2017-07-24 18:54:24 +0200

    testing env

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/info', function(req,res) {
+    res.send('user' + process.env.MONGODB_USER);
+})
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
